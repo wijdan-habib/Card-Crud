0b2185df3a21909bf9363b2261c3a5a8d2a592c3
Wijdan habib <mwijdanamir@gmail.com>
2024-07-14 14:44:09 -0700

    not touched yet

---
+++ src/components/CardComp.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+export default function CardComp() {
+    const [card, setCard] = useState();
+    try {
+        const displayCard = axios
+    } catch (error) {
+        
+    }
+  return (
+    <div>
+
+    </div>
+  )
+}
